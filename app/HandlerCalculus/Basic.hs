57dec805854a35c8f8b67985f4a1af1545e644b4
uenot <tobyueno@gmail.com>
2024-10-14 17:26:38 -0400

    basic typechecking for hc

--- app/HandlerCalculus/Basic.hs
+++ app/HandlerCalculus/Basic.hs
@@ -1,7 +1,6 @@
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE GADTs #-}
-
 module HandlerCalculus.Basic where
+
+import Debug.Trace (trace)
 
 data Kind = ValueKind | CompKind | EffectKind
   deriving (Show)
@@ -11,6 +10,41 @@ data Type
   | HandlerType Type Type
   | CompType Type Type
   | EffectType [(String, ([Type], Type))]
+
+instance Show Type where
+  show ZeroType = "0"
+  show (HandlerType (CompType (EffectType []) ZeroType) (CompType (EffectType []) ZeroType)) = "1"
+  show (HandlerType c d)
+    | show c == "1" = "{" ++ show d ++ "}"
+    | otherwise = parenthesizeShort c ++ " => " ++ parenthesizeShort d
+    where
+      parenthesizeShort :: (Show a) => a -> String
+      parenthesizeShort x
+        | length (show x) < 30 = show x
+        | otherwise = "(" ++ show x ++ ")"
+  show (CompType (EffectType []) a) = show a
+  show (CompType e a) = show e ++ show a
+  show (EffectType ls) = "[" ++ showEffectType ls
+    where
+      showEffectType :: [(String, ([Type], Type))] -> String
+      showEffectType [] = "]"
+      showEffectType [(op, (params, ret))] = op ++ ": (" ++ listParams params ++ ") -> " ++ show ret ++ "]"
+      showEffectType ((op, (params, ret)) : tl) = op ++ ": (" ++ listParams params ++ ") -> " ++ show ret ++ "; " ++ show tl
+
+      listParams :: [Type] -> String
+      listParams [] = ""
+      listParams [hd] = show hd
+      listParams (hd : tl) = show hd ++ ", " ++ listParams tl
+
+data Term
+  = Var String
+  | Handler (String, Term) [(String, ([String], String, Term))]
+  | Absurd Term
+  | Return Term
+  | Let String Type Term Term
+  | Perform String [Term]
+  | With Term Term Type
+  deriving (Show)
 
 kindcheck :: Type -> Maybe Kind
 kindcheck ZeroType = return ValueKind
@@ -40,3 +74,73 @@ kindcheck (CompType e v) = do
   case (ke, kv) of
     (EffectKind, ValueKind) -> return CompKind
     _ -> Nothing
+
+type TypeContext = [(String, Type)]
+
+type EffectContext = [(String, ([Type], Type))]
+
+type Context = (TypeContext, EffectContext)
+
+addToTC :: Context -> String -> Type -> Context
+addToTC (tpctx, efctx) x tx = ((x, tx) : tpctx, efctx)
+
+getEC :: Context -> Type
+getEC = EffectType . snd
+
+addToEC :: Context -> [(String, ([Type], Type))] -> Context
+addToEC (tpctx, efctx) e = (tpctx, e ++ efctx)
+
+lookupEC :: Context -> String -> Maybe ([Type], Type)
+lookupEC ctx name = lookup name (snd ctx)
+
+unwrapComp :: Type -> Maybe ([(String, ([Type], Type))], Type)
+unwrapComp t = case t of
+  CompType (EffectType effects) value -> return (effects, value)
+  _ -> Nothing
+
+typesEqual :: Type -> Type -> Maybe ()
+typesEqual ZeroType ZeroType = return ()
+typesEqual (HandlerType a b) (HandlerType c d) = typesEqual a c >> typesEqual b d
+typesEqual (CompType e a) (CompType f b) = typesEqual e f >> typesEqual a b
+typesEqual (EffectType []) (EffectType []) = return ()
+typesEqual (EffectType (ahd : atl)) (EffectType bls) = do
+  let (op, (aParamTypes, aRetType)) = ahd
+  (bParamTypes, bRetType) <- lookup op bls
+  mapM_ (uncurry typesEqual) (zip aParamTypes bParamTypes)
+  typesEqual aRetType bRetType
+  typesEqual (EffectType atl) (EffectType $ filter ((/= op) . fst) bls)
+typesEqual _ _ = Nothing
+
+typecheck :: TypeContext -> Term -> Type -> Maybe ()
+typecheck ctx (Var x) expectedType = do
+  tx <- lookup x ctx
+  typesEqual tx expectedType
+typecheck ctx (Absurd x) _ = typecheck ctx x ZeroType
+typecheck ctx (Return x) expectedType = do
+  (_, expectedValueType) <- unwrapComp expectedType
+  typecheck ctx x expectedValueType
+typecheck ctx (Let x tx c1 c2) expectedType = do
+  (e, _) <- unwrapComp expectedType
+  typecheck ctx c1 (CompType (EffectType e) tx)
+  typecheck ((x, tx) : ctx) c2 expectedType
+typecheck ctx (Perform op args) expectedType = do
+  (e, b) <- unwrapComp expectedType
+  (expectedArgTypes, retType) <- lookup op e
+  typesEqual b retType
+  mapM_ (uncurry $ typecheck ctx) $ zip args expectedArgTypes
+typecheck ctx (With h m tm) expectedType = do
+  typecheck ctx m tm
+  typecheck ctx h (HandlerType tm expectedType)
+typecheck ctx (Handler retClause opClauses) (HandlerType c d) = do
+  (tcEffects, tcReturn) <- unwrapComp c
+  let (retParam, retBody) = retClause
+  typecheck ((retParam, tcReturn) : ctx) retBody d
+  mapM_ (checkOps tcEffects d) opClauses
+  where
+    checkOps :: [(String, ([Type], Type))] -> Type -> (String, ([String], String, Term)) -> Maybe ()
+    checkOps effs d (op, (args, r, body)) = do
+      (argTypes, retType) <- lookup op effs
+      let contType = HandlerType retType d
+      let ctx' = zip args argTypes ++ (r, contType) : ctx
+      typecheck ctx' body d
+typecheck _ (Handler _ _) _ = Nothing
--- app/HandlerCalculus/Test.hs
+++ app/HandlerCalculus/Test.hs
@@ -4,7 +4,32 @@ import HandlerCalculus.Basic
 
 test :: IO ()
 test = do
-  let zeroComp = CompType (EffectType []) ZeroType
-  let tp = HandlerType zeroComp zeroComp
-  let k = kindcheck tp
-  putStrLn $ show k
+  let compify = CompType (EffectType [])
+  let zeroComp = compify ZeroType
+
+  let unitType = HandlerType zeroComp zeroComp
+  let unit = Handler ("x", Return $ Var "x") []
+
+  let thunkifyType = HandlerType (compify unitType)
+  let thunkifyTerm t = Handler ("_", t) []
+
+  let unitPairType = thunkifyType $ CompType (EffectType [("pair", ([unitType, unitType], ZeroType))]) ZeroType
+  let unitPair = thunkifyTerm $ Perform "pair" [unit, unit]
+
+  let bot = Handler ("x", Absurd $ Var "x")
+  let unthunk v = With v (Return unit) (compify unitType)
+  let unthunkedUnitPairType = CompType (EffectType [("pair", ([unitType, unitType], ZeroType))]) ZeroType
+
+  let first = With (bot [("pair", (["x1", "x2"], "_", Return $ Var "x1"))]) (unthunk unitPair) unthunkedUnitPairType
+
+  test_ first (compify unitType)
+
+test_ :: Term -> Type -> IO ()
+test_ term tp = do
+  putStrLn $ "Term: " ++ show term
+  putStrLn $ "Expected type: " ++ show tp
+  case typecheck [] term tp of
+    Just () -> do
+      putStrLn "TYPECHECKING PASSED"
+      putStrLn $ "Kind: " ++ show (kindcheck tp)
+    _ -> putStrLn "TYPECHECKING FAILED"
